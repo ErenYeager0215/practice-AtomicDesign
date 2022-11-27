be734bfbed2fb703a6f4513d4ed4353cc5a0ca26
エレン <92411701+ErenYeager0215@users.noreply.github.com>
2022-11-27 18:30:07 +0900

    Initial commit

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SContainer>
+      <h2>TOPトップページです</h2>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
